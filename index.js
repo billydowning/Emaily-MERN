4eaf4181eba58269c0b0b08c32f59cff3af2e47b
Billy Downing <wldowning@gmail.com>
2020-11-18 07:07:13 -0700

    wiring up passport

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ sup: 'now?!' });
-});
+passport.use(new GoogleStrategy());
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

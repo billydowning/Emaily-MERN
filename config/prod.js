801f691313bd1f1d0eafe4e5620213c0b28d9ae1
Billy Downing <wldowning@gmail.com>
2020-11-20 12:27:24 -0700

    set up keys

--- config/prod.js
+++ config/prod.js
@@ -6,12 +6,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
-
-// module.exports = {
-//   googleClientID:
-//     "1092653631225-c0ubhuo0nr3l6mj6aqonickrkq9i6qni.apps.googleusercontent.com",
-//   googleClientSecret: "1jwaZmkkBbJClHt7cOI6_1EB",
-//   mongoURI:
-//     "mongodb+srv://wldowning:pUpwhYxsg35tBHH@cluster0.z71hm.mongodb.net/emaily-prod?retryWrites=true&w=majority",
-//   cookieKey: "kdsgksbdgkdsbgDBDBIUD9898689",
-// };

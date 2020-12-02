4ba62307654ce2a3b8bad82b8b26ff19b0dba9f2
Billy Downing <wldowning@gmail.com>
2020-12-02 12:58:57 -0700

    mailer constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,4 +2,20 @@ const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email("billy@yrlang.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email();
+    });
+  }
+}
+
+module.exports = Mailer;

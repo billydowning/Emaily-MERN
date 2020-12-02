2036bc71f827b667a1b4fa5474af7a200942acda
Billy Downing <wldowning@gmail.com>
2020-12-01 18:09:29 -0700

    added mailer object

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
-const Survey = mongoose.model('surveys');
+const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
   app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
@@ -12,7 +12,9 @@ module.exports = (app) => {
       title,
       subject,
       body,
-      recipients: recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      recipients: recipients
+        .split(",")
+        .map((email) => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now(),
     });

db44be93e61e7cc5c4f605260bbe88ff8c8dfbac
Billy Downing <wldowning@gmail.com>
2020-12-03 10:37:51 -0700

    mailer config

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,10 +10,30 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
   }
+
   formatAddresses(recipients) {
     return recipients.map(({ email }) => {
       return new helper.Email();
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipents.forEach((recipient) => {
+      personalize.addTo(recipient);
     });
   }
 }

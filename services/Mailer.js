277b9de7d63bdccf65b3b2bf598c25387cfa4e17
Billy Downing <wldowning@gmail.com>
2020-12-03 13:31:05 -0700

    Mailer confid cont.

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email("billy@yrlang.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
@@ -34,6 +35,15 @@ class Mailer extends helper.Mail {
     const personalize = new helper.Personalization();
     this.recipents.forEach((recipient) => {
       personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: this.toJSON(),
     });
   }
 }

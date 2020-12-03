48608b08f8fa55a7828eb9080c328cb4b19ad569
Billy Downing <wldowning@gmail.com>
2020-12-03 16:00:16 -0700

    sendgrid config

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,10 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
+// Dev only axios helpers
+import axios from "axios";
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
--- services/Mailer.js
+++ services/Mailer.js
@@ -46,7 +46,8 @@ class Mailer extends helper.Mail {
       path: "/v3/mail/send",
       body: this.toJSON(),
     });
-    const response = this.sgApi.API(request);
+
+    const response = await this.sgApi.API(request);
     return response;
   }
 }

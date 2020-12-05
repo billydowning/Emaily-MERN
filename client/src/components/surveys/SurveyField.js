5ac10d7d19d562409957af265cbe3243841f0c00
Billy Downing <wldowning@gmail.com>
2020-12-04 18:09:12 -0700

    survey form config

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,13 @@
+// Contains logic to render a single label and text input
+import React from "react";
+
+const SurveyField = ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
+
+export default SurveyField;
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,46 @@
+import _ from "lodash";
 import React from "react";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" },
+];
 
 class SurveyForm extends React.Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <h4>Survey Form</h4>
+        <form
+          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+        >
+          {this.renderFields()}
+          <button type="submit" className="ui primary button">
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
 }
 
-export default SurveyForm;
+export default reduxForm({
+  form: "surveyForm",
+})(SurveyForm);

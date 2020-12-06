5be2688a4135a83f36a0cbd1b24cf1fca025e57e
Billy Downing <wldowning@gmail.com>
2020-12-05 17:34:58 -0700

    added field validation

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,12 @@
 // Contains logic to render a single label and text input
 import React from "react";
 
-const SurveyField = ({ input, label, meta }) => {
+const SurveyField = ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   );
 };

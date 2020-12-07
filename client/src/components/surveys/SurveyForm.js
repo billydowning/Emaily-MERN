9c2310bdc746c0eea8a149552d92b8f68e83e152
Billy Downing <wldowning@gmail.com>
2020-12-06 18:25:28 -0700

    persisting form values

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -84,4 +84,5 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: "surveyForm",
+  destroyOnUnmount: false,
 })(SurveyForm);
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,50 @@
+//SurveyFormReview shows users form input values for review
 import React from "react";
+import { connect } from "react-redux";
 
-const SurveyFormReview = () => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
   return (
     <div>
-      <h5>Please confirm your entries</h5>
+      <h5>Please confirm your entries below:</h5>
+      <div className="ui list">
+        <div className="item">
+          <i className="envelope icon"></i>
+          <div className="content">
+            <label>Survey Title:</label>
+            <div>{formValues.title}</div>
+          </div>
+        </div>
+        <div className="item">
+          <i className="envelope icon"></i>
+          <div className="content">
+            <label>Subject:</label>
+            <div className="content">{formValues.subject}</div>
+          </div>
+        </div>
+        <div className="item">
+          <i className="envelope icon"></i>
+          <div className="content">
+            <label>Email body:</label>
+            <div className="content">{formValues.body}</div>
+          </div>
+        </div>
+        <div className="item">
+          <i className="envelope icon"></i>
+          <div className="content">
+            <label>Recipients:</label>
+            <div className="content">{formValues.emails}</div>
+          </div>
+        </div>
+      </div>
+      <button className="ui primary button" onClick={onCancel}>
+        Back
+      </button>
     </div>
   );
 };
 
-export default SurveyFormReview;
+const mapStateToProps = (state) => {
+  return { formValues: state.form.surveyForm.values };
+};
+
+export default connect(mapStateToProps)(SurveyFormReview);
